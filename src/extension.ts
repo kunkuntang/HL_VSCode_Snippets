f351b08ad120f19d7091b90e5e4c9f3d7baab931
Kuntang Huang <kuntang@163.com>
2020-03-18 14:25:02 +0800

    feat: 完成model创建的代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,62 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
-import { ModelCreator } from './modelCreator';
-import { showInputBox } from './baseInput';
+import * as vscode from "vscode";
+import { ModelCreator } from "./modelCreator";
+import { showInputBox } from "./baseInput";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.createModel', async function () {
-		let inputClassName = await showInputBox()
-		// Get the active text editor
-		let editor = vscode.window.activeTextEditor;
+  let disposable = vscode.commands.registerCommand(
+    "extension.createModel",
+    async function() {
+      let inputClassName = await showInputBox();
+      // Get the active text editor
+      let editor = vscode.window.activeTextEditor;
 
-		// console.log('clipboard', await vscode.env.clipboard.readText())
-		const clipboardText = await vscode.env.clipboard.readText()
-		const modelCreator = new ModelCreator(inputClassName || '', clipboardText)
+      const clipboardText = await vscode.env.clipboard.readText();
+      let safeJsonData = {};
+      try {
+        safeJsonData = JSON.parse(clipboardText);
 
-		const resultStr = modelCreator.generateModel()
-		// console.log('resultStr', resultStr)
+        const modelCreator = new ModelCreator(
+          inputClassName || "",
+          safeJsonData
+        );
 
-		if (editor) {
-			let document = editor.document;
-			let selection = editor.selection;
+        const resultStr = modelCreator.generateModel();
+        if (editor) {
+          let selection = editor.selection;
 
-			// Get the word within the selection
-			let word = document.getText(selection);
-			editor.edit(editBuilder => {
-				editBuilder.insert(selection.active, resultStr);
-			});
-		}
-	});
+          // Get the word within the selection
+          editor.edit(editBuilder => {
+            editBuilder.insert(selection.active, resultStr);
+          });
+        }
+      } catch (error) {
+        vscode.window.showErrorMessage(
+          "非法的JSON字符串，请重新复制接口响应数据"
+        );
+        let result = await vscode.window.showQuickPick(["是", "否"], {
+          placeHolder: "是否继续生成model？"
+        });
+        if (result === "是") {
+          const modelCreator = new ModelCreator(
+            inputClassName || "",
+            safeJsonData
+          );
 
-	context.subscriptions.push(disposable);
+          const resultStr = modelCreator.generateModel();
+          if (editor) {
+            let selection = editor.selection;
+
+            // Get the word within the selection
+            editor.edit(editBuilder => {
+              editBuilder.insert(selection.active, resultStr);
+            });
+          }
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
